4e22ef80feffd9e70dfe1669386b24ebf1d4ffe1
Abir Chodha <abirchodha1@gmail.com>
2024-06-08 13:07:49 +0530

    Use top-level await (only for modules) This will also block the code, the code after await executes only after the fetch is complete

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 add('pizza', 2);
 console.log(price);
 console.log(tq);
+
+const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+const data = await res.json();
+console.log(data);
