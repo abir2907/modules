1f6731d9caf8f2a97d6e387511f9bea4c6ba91cd
Abir Chodha <abirchodha1@gmail.com>
2024-06-08 13:17:20 +0530

    Good example of top level await

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ add('pizza', 2);
 console.log(price);
 console.log(tq);
 
-const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-const data = await res.json();
-console.log(data);
+const getLastPost = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+// Incorrect, this will print the Promise(pending) and not the object
+
+lastPost.then(last => console.log(last));
+// Correct
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+// Correct, looks cleaner
